dd27c23d0d67c8ce95fbfee2c2f1b27e55e63bf2
perfe-er <zhu063296@gmail.com>
2019-11-21 22:53:09 +0800

    1.创建主布局文件，关联按钮,创建BeatBoxFragment,实现BeatBoxActivity继承ingleFragmentActivity 2.使用数据绑定（app/build.gradle），把一般布局改造为数据绑定布局，BeatBoxFragment.java中实例化绑定类并配置RecyclerView 3.创建声音布局文件，BeatBoxFragment.java中：创建SoundHolder和SoundAdapter，使用SoundAdapter 4.导入assets，创建BeatBox类，获取AssetManager，查看assets资源，BeatBoxFragment.java中创建BeatBox实例，释放BeatBox 5.创建Sound对象，BeatBox.java中创建Sound列表，BeatBoxFragment.java中绑定Sound列表并传入声音资源，创建SoundPool对象，释放SoundPool，加载音频，在BeatBox中添加play(Sound)方法 6.运用MVVM架构，创建SoundViewModel类，添加绑定方法，声音列表中声明视图模型属性并绑定按钮文件名 7.BeatBoxFragment.java关联使用视图模型，调用bind(Sound)方法 8.使用数据绑定的BaseObservable类 9.添加测试依赖，创建测试类，创建虚拟BeatBox对象，创建SoundViewModel测试对象，添加测试标题属性，创建onButtonClicked()方法，实现onButtonClicked()方法，调用onButtonClicked()方法，验证BeatBox.play(Sound)方法 10.调用setRetainInstance(true)

--- app/src/test/java/com/example/zwq/beatbox/SoundViewModelTest.java
+++ app/src/test/java/com/example/zwq/beatbox/SoundViewModelTest.java
@@ -7,7 +7,6 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 public class SoundViewModelTest {
     private BeatBox mBeatBox;
